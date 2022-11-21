772d7d43c4dea3e6329b4bc353dccfd7b2000e8f
Courtney Yatteau <cyatteau@esri.com>
2022-11-21 16:10:45 -0500

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,28 @@
+import { map, currentMarkers } from "./main";
+const apiKey = "YOUR_API_KEY";
+import { geocode } from "esri-leaflet-geocoder";
+
+export function showPlaces() {
+  let position;
+  geocode({
+    apikey: apiKey,
+  })
+    .category("Post Office")
+    .nearby(map.getCenter(), 1)
+    .run(function(err, response) {
+      for (const result of response.results) {
+        position = new L.LatLng(result.latlng.lat, result.latlng.lng);
+        currentMarkers.push(
+          new L.marker(position).addTo(map).bindTooltip(() => {
+            return L.Util.template(
+              `<b>${result.properties.PlaceName}</b><br/>
+              ${result.properties.Place_addr}<br/>
+              ${result.properties.Phone}<br/>
+              ${result.properties.URL}`
+            );
+          })
+        );
+      }
+    });
+  map.setView(position, 14);
+}
